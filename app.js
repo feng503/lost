5b6f7e58018c9e205a030d49e06a2dae7e48a438
root <1258252786@qq.com>
2020-10-01 11:34:08 +0800

    忘了改后端地址

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //app.js
 App({
-  // serverUrl: "https://www.takeithome.top/lost/",
-  serverUrl: "http://127.0.0.1:8700/lost/",
+  serverUrl: "https://www.takeithome.top/lost/",
+  // serverUrl: "http://127.0.0.1:8700/lost/",
   globalData: {
     newlist: [],
     id: [],
